a3aa6644a3b10363d519a07eb9a110ac3e58fc0d
gerry <krittameth2020@gmail.com>
2025-10-01 12:25:53 +0700

    Updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module se233.notcontra {
     requires javafx.controls;
     requires javafx.fxml;
-
+	requires javafx.graphics;
 
     opens se233.notcontra to javafx.fxml;
     exports se233.notcontra;
